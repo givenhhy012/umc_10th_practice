618f2debe55a9624e53595b41508c533b1e7b2b3
{YANG JUNHYEOK} <wnsgur5152@kw.ac.kr>
2026-04-05 13:51:12 +0900

    프로젝트 초기 세팅

---
+++ src/main/java/com/example/umc10th/domain/user/exception/MemberException.java
@@ -0,0 +1,7 @@
+package com.example.umc10th.domain.user.exception;
+
+public class MemberException extends RuntimeException {
+    public MemberException(String message) {
+        super(message);
+    }
+}
